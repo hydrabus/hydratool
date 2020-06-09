559df7cf9c2b03f7d349da53b2107f4497e449f1
bvernoux <bvernoux@gmail.com>
2020-06-09 12:03:55 +0200

    HydraTool v0.3.2.1

--- version.h
+++ version.h
@@ -1,19 +1,19 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_DATE_INFO_STR           " - 18 Jan 2018"
+#define VER_DATE_INFO_STR           " - 09 Jun 2020"
 
-#define VER_FILEVERSION             0,3,2,0
-#define VER_FILEVERSION_STR         "0.3.2.0\0"
+#define VER_FILEVERSION             0,3,2,1
+#define VER_FILEVERSION_STR         "0.3.2.1\0"
 
-#define VER_PRODUCTVERSION          0,3,2,0
+#define VER_PRODUCTVERSION          0,3,2,1
 #define VER_PRODUCTVERSION_STR      "0.3\0"
 
 #define VER_COMPANYNAME_STR         "hydrabus"
 #define VER_FILEDESCRIPTION_STR     "hydratool"
 #define VER_INTERNALNAME_STR        "hydratool"
-#define VER_LEGALCOPYRIGHT_STR      "B.VERNOUX (c)2018"
-#define VER_LEGALTRADEMARKS1_STR    "B.VERNOUX (c)2018"
+#define VER_LEGALCOPYRIGHT_STR      "B.VERNOUX (c)2018-2020"
+#define VER_LEGALTRADEMARKS1_STR    "B.VERNOUX (c)2018-2020"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "hydratool.exe"
 #define VER_PRODUCTNAME_STR         "hydratool"
